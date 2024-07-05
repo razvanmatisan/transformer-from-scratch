774a6739ebfe8238f555463b9ef87379d755b851
Razvan Matisan <razvanmatisan@gmail.com>
2024-07-05 10:44:15 +0200

    Implement FFN sublayer

--- model/sublayers.py
+++ model/sublayers.py
@@ -4,10 +4,7 @@ import matplotlib.pyplot as plt
 import math
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self,
-            heads: int = 8,
-            d_model: int = 128,
-    ):
+    def __init__(self, heads: int = 8, d_model: int = 512):
         super(MultiHeadAttention, self).__init__()
 
         assert d_model % heads == 0, "Embedding dimension must be divisible by the number of attention heads!"
@@ -16,21 +13,14 @@ class MultiHeadAttention(nn.Module):
         self.d_model = d_model
 
         self.d_k = d_model // heads
-        self.d_v = d_model // heads
 
-        self.q_proj = nn.Linear(d_model, d_model)
-        self.k_proj = nn.Linear(d_model, d_model)
-        self.v_proj = nn.Linear(d_model, d_model)
+        self.qkv_proj = nn.Linear(d_model, 3 * d_model)
         self.o_proj = nn.Linear(d_model, d_model)
 
     def _reset_parameters(self):
         # Original Transformer initialization, see PyTorch documentation
-        nn.init.xavier_uniform_(self.q_proj.weight)
-        nn.init.xavier_uniform_(self.k_proj.weight)
-        nn.init.xavier_uniform_(self.v_proj.weight)
-        self.q_proj.bias.data.fill_(0)
-        self.k_proj.bias.data.fill_(0)
-        self.v_proj.bias.data.fill_(0)
+        nn.init.xavier_uniform_(self.qkv_proj.weight)
+        self.qkv_proj.bias.data.fill_(0)
 
         nn.init.xavier_uniform_(self.o_proj.weight)
         self.o_proj.bias.data.fill_(0)
@@ -53,16 +43,13 @@ class MultiHeadAttention(nn.Module):
     def forward(self, x):
         bs, seq_len, _ = x.shape
 
-        # (bs, seq_len, d_model)
-        q = self.q_proj(x) 
-        k = self.k_proj(x)
-        v = self.v_proj(x) 
+        # (bs, seq_len, 3 * d_model)
+        qkv = self.qkv_proj(x)
 
-        # (bs, heads, seq_len, d_k)
-        q = q.reshape(bs, seq_len, self.heads, self.d_k).permute(0, 2, 1, 3)
-        k = k.reshape(bs, seq_len, self.heads, self.d_k).permute(0, 2, 1, 3)
-        v = v.reshape(bs, seq_len, self.heads, self.d_k).permute(0, 2, 1, 3)
-        
+        # (bs, heads, seq_len, 3 * d_k) --> 3 x (bs, heads, seq_len, d_k)
+        qkv = qkv.reshape(bs, seq_len, self.heads, 3 * self.d_k).permute(0, 2, 1, 3)
+        q, k, v = torch.split(qkv, self.d_k, dim=-1)
+
         # (bs, heads, seq_len, d_v)
         attentions = self.scaled_dot_product(q, k, v)
         
@@ -72,14 +59,31 @@ class MultiHeadAttention(nn.Module):
         
         return out
     
+class FFN(nn.Module):
+    def __init__(self, d_model: int = 512, d_hidden: int = 2048):
+        super(FFN, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(d_model, d_hidden),
+            nn.ReLU(),
+            nn.Linear(d_hidden, d_model)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
 
 if __name__ == "__main__":
-    bs = 2
+    bs = 1
     seq_len = 10
     n_heads = 8
     d_model = 128
+    d_hidden = 256
 
-    MHA = MultiHeadAttention(n_head=n_heads, d_model=d_model)
     x = torch.randn(bs, seq_len, d_model)
 
-    print(MHA(x).shape)
+    mha = MultiHeadAttention(heads=n_heads, d_model=d_model)
+    attentions = mha(x)
+
+    ffn = FFN(d_model=d_model, d_hidden=d_hidden)
+
+    print(ffn(attentions).shape)
